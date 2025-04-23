a4e7452c436f57eec80feb2cb67fffe0eda72c6a
snackerman12 <harperjamesdean@gmail.com>
2025-04-23 11:18:18 -0500

    test snackman12

--- src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // test commit
-// firstBlock test
+// firstBlock
 public class CaastleAndSnackman1sEndMod implements ModInitializer {
 	public static final String MOD_ID = "csendmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
