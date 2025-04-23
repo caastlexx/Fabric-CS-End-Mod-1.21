ecbe4edaa0e24a4fef21b726c1cec5e2611e63c0
caastlexx <michaeln512296@gmail.com>
2025-04-23 10:22:15 -0500

    reset

--- src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 // test commit
 
-//what up
 public class CaastleAndSnackman1sEndMod implements ModInitializer {
 	public static final String MOD_ID = "csendmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
