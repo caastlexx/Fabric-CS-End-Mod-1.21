5d2eef157990628118657286813e5cc352bc2880
caastlexx <michaeln512296@gmail.com>
2025-04-23 09:49:47 -0500

    test commit

--- src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// test commit
 public class CaastleAndSnackman1sEndMod implements ModInitializer {
 	public static final String MOD_ID = "csendmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
