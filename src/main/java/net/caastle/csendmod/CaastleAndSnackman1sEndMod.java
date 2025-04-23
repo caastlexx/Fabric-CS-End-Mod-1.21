94d8851b8f2a3ca7c2acb09bd2bb03a017084345
caastlexx <michaeln512296@gmail.com>
2025-04-23 09:47:11 -0500

    Initial commit

---
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
@@ -0,0 +1,15 @@
+package net.caastle.csendmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CaastleAndSnackman1sEndMod implements ModInitializer {
+	public static final String MOD_ID = "csendmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndModClient.java
@@ -0,0 +1,10 @@
+package net.caastle.csendmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CaastleAndSnackman1sEndModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
