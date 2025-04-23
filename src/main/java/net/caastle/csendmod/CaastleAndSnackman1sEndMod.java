e4608d4e0b57e5f12972f8ee8d542d4893dbbc3d
snackerman12 <harperjamesdean@gmail.com>
2025-04-23 11:51:55 -0500

    sigma

--- src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndMod.java
@@ -5,9 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// test commit
 // firstBlock
-// test commit 2
-
 public class CaastleAndSnackman1sEndMod implements ModInitializer {
 	public static final String MOD_ID = "csendmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
