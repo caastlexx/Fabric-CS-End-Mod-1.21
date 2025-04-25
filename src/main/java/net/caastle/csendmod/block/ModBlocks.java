0fd8b1a7361025edae3e79deac401f64d556cbf0
snackerman12 <harperjamesdean@gmail.com>
2025-04-25 10:01:32 -0500

    fixed master

--- src/main/java/net/caastle/csendmod/block/ModBlocks.java
+++ src/main/java/net/caastle/csendmod/block/ModBlocks.java
@@ -1,10 +1,12 @@
 package net.caastle.csendmod.block;
 
 import net.caastle.csendmod.CaastleAndSnackman1sEndMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -28,5 +30,9 @@ public class ModBlocks {
 
     public static void registerModBlocks(){
         CaastleAndSnackman1sEndMod.LOGGER.info("Registering Mod Blocks for " + CaastleAndSnackman1sEndMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.VOIDIUM_ORE_BLOCK);
+        });
     }
 }
