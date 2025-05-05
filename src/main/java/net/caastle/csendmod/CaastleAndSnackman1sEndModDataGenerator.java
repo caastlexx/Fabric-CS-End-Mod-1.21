9813f4dfa32e951ab2cdd959cfa8f8ad5a0ec662
caastlexx <michaeln512296@gmail.com>
2025-05-05 12:45:34 -0500

    world gen setup

--- src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndModDataGenerator.java
+++ src/main/java/net/caastle/csendmod/CaastleAndSnackman1sEndModDataGenerator.java
@@ -1,8 +1,12 @@
 package net.caastle.csendmod;
 
 import net.caastle.csendmod.datagen.*;
+import net.caastle.csendmod.world.ModConfiguredFeatures;
+import net.caastle.csendmod.world.ModPlacedFeatures;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class CaastleAndSnackman1sEndModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -14,5 +18,13 @@ public class CaastleAndSnackman1sEndModDataGenerator implements DataGeneratorEnt
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModRegistryDataGenerator::new);
+
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 	}
 }
